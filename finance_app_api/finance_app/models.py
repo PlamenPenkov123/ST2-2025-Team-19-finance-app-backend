5fd74e90639a08a51dba8a4e349e6af8e4d7e2a7
PlamenPenkov123 <plamenpenkov29@gmail.com>
2025-11-06 13:48:37 +0200

    fix

--- finance_app_api/finance_app/models.py
+++ finance_app_api/finance_app/models.py
@@ -17,11 +17,7 @@ class User(AbstractUser):
 
 class Budget(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='budgets')
-    amount = models.DecimalField(
-        max_digits=12,
-        decimal_places=2,
-        validators=[MinValueValidator(Decimal('0.00'))]
-    )
+    amount = models.FloatField()
     current_amount = models.DecimalField(
         max_digits=12,
         decimal_places=2,
